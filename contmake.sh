e9ba99de5bae74509c6bbdd9dd2c1860d28ffd0c
ottyajp <ottyajp@gmail.com>
2017-03-10 17:25:30 +0900

    取り敢えず基本の部分

--- contmake.sh
+++ contmake.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 
-echo $1
+echo input: $1 $2
+
+if [[ $1 = "make" ]];then
+  echo make $2
+  mkdir $2
+  sudo pacstrap -i -c -d ./$2 base base-devel --ignore linux
+fi
+
+if [[ $1 = "boot" ]];then
+  echo boot $2
+  sudo systemd-nspawn -bD $2
+fi
+
